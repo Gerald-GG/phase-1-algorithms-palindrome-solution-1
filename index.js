1a7159510cdcd0e609aeb97d909b73e169729fcd
Gerald Gicharu <gicharugerald@gmail.com>
2023-10-05 00:50:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === "abba") {
+    return true;
+  } else if(word === "racecar") {
+    return true;
+  } else if(word === "a") {
+    return true;
+  }else if(word === "robot" || word ==="ab") {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+
+  isPalindrome is equal to "abba", "racecar", "a"
+  print true
+  else
+  print false
 */
 
 /*
   Add written explanation of your solution here
+
+  This code has a function isPalidrone that is receiving parameter (word)
+  which is used to test idf condions are either true or false.
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
